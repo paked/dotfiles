435e8a9342d52e5fc5f420b4397e8796bbeed43c
Harrison Shoebridge <harrison@theshoebridges.com>
2017-04-30 16:58:15 -0700

    Add ngrok alias

--- .alias.zsh
+++ .alias.zsh
@@ -1,7 +1,15 @@
+export DMNG_PORT=3000
+
+# Docker
 alias dme='eval $(docker-machine env)'
+alias dmng='ngrok http -subdomain=harrison $(docker-machine ip):$DMNG_PORT'
 alias dcu='docker-compose run'
 alias dcus='docker-compose run --service-ports'
+
+# GNU/Utils
 alias lsa='ls -a'
+
+# Git
 alias gs='git status'
 alias ga='git add -p'
 alias gc='git commit'
