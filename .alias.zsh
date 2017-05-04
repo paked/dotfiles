4cc463049415f87e8a1627776acf7b5b228ffbf3
Harrison Shoebridge <harrison@theshoebridges.com>
2017-05-04 01:43:08 -0700

    Add more aliases

--- .alias.zsh
+++ .alias.zsh
@@ -3,9 +3,10 @@ alias ae="$EDITOR ~/.alias.zsh && source ~/.alias.zsh"
 
 # Docker
 alias dme='eval $(docker-machine env)'
-alias ng='ngrok http -subdomain=harrison'
 alias dcu='docker-compose run'
 alias dcus='docker-compose run --service-ports'
+
+alias ng='ngrok http -subdomain=harrison'
 
 # GNU/Utils
 alias lsa='ls -a'
@@ -15,6 +16,7 @@ alias gs='git status'
 alias ga='git add -p'
 alias gc='git commit'
 alias gp='git push'
+alias gl='git log'
 alias gpu='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
 
 # Navigation
@@ -24,3 +26,5 @@ alias hca='hc && cd api'
 alias hcf='hc && cd frontend'
 alias hcw='hc && cd website'
 alias dfs='code && cd dotfiles'
+
+alias hcsd='hc && tmux new-session -s hc'
