5bed934928d24dbc939578ba51dca1b1b5ddfb15
Harrison Shoebridge <harrison@theshoebridges.com>
2017-05-12 15:34:57 -0700

    Fix network name

--- .blocks/network.rb
+++ .blocks/network.rb
@@ -15,15 +15,23 @@ device = ENV['BLOCK_INSTANCE']
 network_status = `nmcli --terse --fields device,state dev status`
 
 def state?(device, state)
-  /#{device}:#{state}/
+  /#{device}:#{state}\n/
+end
+
+def network_name(device)
+  out = `nmcli --terse --fields NAME,DEVICE connection show --active`.strip
+
+  m = out.match(/(.*)\:#{device}/)
+  return "Unknown" unless m
+
+  m[1]
 end
 
 case network_status
 when state?(device, STATUS_CONNECTED)
   strength = `grep "#{device}" /proc/net/wireless | awk '{ print int($3) * 100/70 }'`.strip.to_i
-  name = `nmcli --terse --fields NAME connection show --active`.strip
 
-  out = "#{ICON_STATUS_CONNECTED} #{name} (#{strength}%)"
+  out = "#{ICON_STATUS_CONNECTED} #{network_name(device)} (#{strength}%)"
 
   if strength > 70
     puts Color.good(out)
